f5d5eea1d83efdb18c48797e30f37d10ec6e2533
Kevin Luo <kevin.luo@siloon.com>
2014-12-05 11:41:10 +0800

    chat init

---
+++ ui/chat-room.reel/chat-room.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/chat-room.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ChatRoom
+ * @extends Component
+ */
+exports.ChatRoom = Component.specialize(/** @lends ChatRoom# */ {
+    constructor: {
+        value: function ChatRoom() {
+            this.super();
+        }
+    }
+});
